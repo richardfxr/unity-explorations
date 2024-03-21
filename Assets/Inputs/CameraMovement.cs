c714d64645fa54af8f89f3ceac482c8bced85ec2
Richard Fu <richardfxr.personal@gmail.com>
2024-03-20 23:23:42 -0400

    add Y-axis camera movement & input smoothing

--- Assets/Inputs/CameraMovement.cs
+++ Assets/Inputs/CameraMovement.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CameraMovement : MonoBehaviour {
+    public GameObject marble;
+    public float moveSmoothDampSpeed;
+    public float cameraOffsetY;
     public float rotateSpeed;
+    public float rotateSmoothDampSpeed;
     public Vector3 initialRotation;
+
     private Vector2 _moveVector;
+    private float _currentRotateY;
+    private float _targetRotateY;
+    private float _rotateSmoothDampVelocity;
+    private float _currentMoveY;
+    private float _moveSmoothDampVelocity;
 
     void Start() {
+        // set up initial rotation
         transform.eulerAngles = initialRotation;
+        _currentRotateY = initialRotation.y;
+        _targetRotateY = initialRotation.y;
     }
 
     void Update() {
+        _targetRotateY -= _moveVector.x * rotateSpeed * Time.deltaTime;
+        _currentRotateY = Mathf.SmoothDamp(_currentRotateY, _targetRotateY, ref _rotateSmoothDampVelocity, rotateSmoothDampSpeed);
+
         transform.eulerAngles = new Vector3(
             transform.eulerAngles.x,
-            transform.eulerAngles.y - _moveVector.x * rotateSpeed * Time.deltaTime,
+            _currentRotateY,
             transform.eulerAngles.z
+        );
+
+        _currentMoveY = Mathf.SmoothDamp(_currentMoveY, marble.transform.position.y + cameraOffsetY, ref _moveSmoothDampVelocity, moveSmoothDampSpeed);
+
+        transform.position = new Vector3(
+            transform.position.x,
+            _currentMoveY,
+            transform.position.z
         );
     }
 
